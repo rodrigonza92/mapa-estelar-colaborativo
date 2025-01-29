482910f33ff565e3056af92973df7e80a8c6570e
Rodrigo <rodrigo.e.gonza@gmail.com>
2025-01-28 22:24:26 -0300

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,8 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import AddObservation from "./components/pages/main/AddObservation";
 import { AuthProvider } from "./context/AuthContext";
 import Validations from "./components/pages/main/Validations";
-<<<<<<< HEAD
 import Profile from "./components/Profile";
-=======
 import ObjectPage from "./components/pages/main/ObjectPage";
->>>>>>> 0bd453deca5058fade83d303f3ebde0b5b96c67a
 
 function App() {
   return (
@@ -22,45 +19,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
-          <Route 
-            path="/main" 
-            element={
-              <ProtectedRoute>
-                <Main />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/add-observation" 
-            element={
-              <ProtectedRoute>
-                <AddObservation />
-              </ProtectedRoute>
-            } 
-          />
-            <Route 
-            path="/validations" 
-            element={
-                <ProtectedRoute>
-                <Validations />
-                </ProtectedRoute>
-            } 
-            />
-            <Route 
-            path="/profile" 
-            element={
-                <ProtectedRoute>
-                <Profile />
-                </ProtectedRoute>
-            } 
-            />
-=======
           <Route path="/main" element={<ProtectedRoute><Main /></ProtectedRoute>}/>
           <Route path="/add-observation" element={<ProtectedRoute><AddObservation /></ProtectedRoute>}/>
           <Route path="/validations" element={<ProtectedRoute><Validations /></ProtectedRoute>}/>
           <Route path="/objects" element={<ProtectedRoute><ObjectPage /></ProtectedRoute>}/>
->>>>>>> 0bd453deca5058fade83d303f3ebde0b5b96c67a
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}/>
         </Routes>
       </Router>
     </AuthProvider>
